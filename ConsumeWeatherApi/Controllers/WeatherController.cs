ac8ef840f4fce93eefafd43523737e0edd58babc
tarak <tarakaram.gasi@gmail.com>
2024-06-14 13:35:55 -0500

    Add project files.

---
+++ ConsumeWeatherApi/Controllers/WeatherController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DataAccessLayer;
+
+namespace ConsumeWeatherApi.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class WeatherController : ControllerBase
+    {
+        private IWeatherServices _services = null!;
+        public WeatherController(IWeatherServices services)
+        {
+            _services = services;
+        }
+        public object Get()
+        {
+            var response = _services.GetWeatherDetails();
+            return response;
+        }
+    }
+}
---
+++ DataAccessLayer/WeatherServices.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+namespace DataAccessLayer
+{
+    public class WeatherServices : IWeatherServices
+    {
+        public object GetWeatherDetails()
+        {
+            HttpClient httpClient = new HttpClient();
+            var url = "http://localhost:5257/WeatherForecast";
+            HttpResponseMessage responseMessage = httpClient.GetAsync(url).Result;
+            var response = JsonConvert.SerializeObject(responseMessage.Content.ReadAsAsync<object>().Result);
+            return response;
+        }
+    }
+}
